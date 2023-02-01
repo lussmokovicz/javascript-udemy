6721236f7bef76db242089776a8e819fea28f27b
Luana Stanicheski Smokovicz <121254943+lussmokovicz@users.noreply.github.com>
2023-02-01 14:04:41 -0300

    update

--- aula4/002.js
+++ aula4/002.js
@@ -3,6 +3,13 @@ console.log('Olá mundo!'); //Aqui o que vem depois das duas barras é ignorado
 console.log('Linha1');
 // Comentário
 console.log('Linha2');
-console.log('Linha3');
+//console.log('Linha3');
 console.log('Linha4');
 // Vai ser ignorado
+
+/*Esse
+é
+o
+código
+de 
+bloco - Funciona para várias linhas*/
